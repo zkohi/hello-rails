6a8765073ce4e836fc9776ea2a8c8b77b4bcd5ec
Hiroyuki Komatsuzawa <kohiandrew@gmail.com>
2017-06-24 00:59:34 +0900

    Clean blogs and comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -69,6 +69,6 @@ class CommentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
-      params.fetch(:comment, {})
+      params.require(:comment).permit(:body, :status, :entry_id)
     end
 end
--- app/views/comments/_comment.json.jbuilder
+++ app/views/comments/_comment.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! comment, :id, :created_at, :updated_at
+json.extract! comment, :id, :body, :status, :entry_id, :created_at, :updated_at
 json.url comment_url(comment, format: :json)
