a236fde01f2e735cb121fcc01a954435b36db813
Hiroyuki Komatsuzawa <kohiandrew@gmail.com>
2017-06-24 00:52:56 +0900

    Mod routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :entries
+  resources :blogs do
+    resources :entries do
+      resources :comments
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
