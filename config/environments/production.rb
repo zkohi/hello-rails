9c82dfb3615e06641d6c00e41d526d5dfc50fe47
Hiroyuki Komatsuzawa <kohiandrew@gmail.com>
2017-07-07 00:27:25 +0900

    Add default_url_options to production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,9 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "hello_rails_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  # default hostname
+  config.action_mailer.default_url_options = { host: 'localhost' }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
